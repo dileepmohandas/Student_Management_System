0ddb2fcf0b82c8b43c4de8b3da647ea88de7848f
dileep <dileepmohandas14@gmail.com>
2024-02-25 00:12:13 +0530

    updated staff

--- student_ms/student_ms/Hod_Views.py
+++ student_ms/student_ms/Hod_Views.py
@@ -239,3 +239,42 @@ def VIEW_STAFF(request):
     print(staff)
     context={'staff':staff}
     return render(request,'Hod/view_staff.html',context)
+
+@login_required(login_url='/')
+def EDIT_STAFF(request,id):
+    staff = Staff.objects.get(id = id)
+    context={"staff":staff}
+    return render(request,'Hod/edit_staff.html',context)
+
+@login_required(login_url='/')
+def UPDATE_STAFF(request):
+    if  request.method == 'POST':
+        staff_id = request.POST.get('staff_id')
+        profile_pic = request.FILES.get('profile_pic')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+        gender = request.POST.get('gender')
+
+        user = CustomUser.objects.get(id = staff_id)
+        user.username = username
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email = email
+
+        if password != None and password != "":
+            user.set_password(password)
+        if profile_pic != None and profile_pic != "":
+            user.profile_pic = profile_pic
+        user.save()
+        staff = Staff.objects.get(admin=staff_id)
+        staff.gender=gender
+        staff.address=address
+        staff.save()
+        messages.success(request,"Staff Is Successfully updated")
+        return redirect('view_staff')
+        
+    return render(request,'Hod/edit_staff.html')
--- student_ms/student_ms/urls.py
+++ student_ms/student_ms/urls.py
@@ -30,13 +30,18 @@ urlpatterns = [
     path('Hod/Home/',Hod_Views.Home,name='hod_home'),
     path('Profile/',views.PROFILE,name='profile'),
     path('Profile/update',views.PROFILE_UPDATE,name='profile_update'),
+    
     path('Hod/Student/Add',Hod_Views.ADD_STUDENT,name='add_student'),
     path('Hod/Student/View',Hod_Views.VIEW_STUDENT,name='view_student'),
     path('Hod/Student/Edit/<str:id>',Hod_Views.EDIT_STUDENT,name='edit_student'),
     path('Hod/Student/Update',Hod_Views.UPDATE_STUDENT,name='update_student'),
     path('Hod/Student/Delete/<str:admin>',Hod_Views.DELETE_STUDENT,name='delete_student'),
+    
     path('Hod/Staff/Add',Hod_Views.ADD_STAFF,name='add_staff'),
     path('Hod/Staff/View',Hod_Views.VIEW_STAFF,name='view_staff'),
+    path('Hod/Staff/Edit/<str:id>',Hod_Views.EDIT_STAFF,name='edit_staff'),
+    path('Hod/Staff/Update',Hod_Views.UPDATE_STAFF,name='update_staff'),
+    
     path('Hod/Course/Add',Hod_Views.ADD_COURSE,name='add_course'),
     path('Hod/Course/View',Hod_Views.VIEW_COURSE,name='view_course'),
     path('Hod/Course/Edit/<str:id>',Hod_Views.EDIT_COURSE,name='edit_course'),
