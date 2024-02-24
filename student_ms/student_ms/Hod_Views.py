9c0231201cc93ae2f5bce9184d3fb540f8c7a023
dileep <dileepmohandas14@gmail.com>
2024-02-24 12:35:13 +0530

    updated course,session

--- student_ms/student_ms/Hod_Views.py
+++ student_ms/student_ms/Hod_Views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-
+from studentapp.models import Course,Session_Year
 
 @login_required(login_url='/')
 def Home(request):
@@ -9,4 +9,13 @@ def Home(request):
 
 @login_required(login_url='/')
 def ADD_STUDENT(request):
-    return render(request,'Hod/add_student.html')
+    course =  Course.objects.all()
+    session_year = Session_Year.objects.all()
+
+    context =  {
+        'course':course , 
+        'session_year' : session_year,
+    }
+    
+        
+    return render(request,'Hod/add_student.html',context)
